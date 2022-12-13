d16cc5301b979f8ceb5f74778b9c9d8b79a721ec
moisescondori02 <moisescondori700@gmail.com>
2022-12-12 21:18:28 -0300

    subiendo tp
    subiendo

---
+++ in.js
@@ -0,0 +1,12 @@
+let edad;
+
+edad = prompt("Introduce tu edad");
+
+
+if ( edad >= 18){
+    alert("Puedes entrar, eres mayor de edad.");
+
+}else{
+    alert(" Eres menor de edad ,entra bajo su responsabilidad..");
+}
+
